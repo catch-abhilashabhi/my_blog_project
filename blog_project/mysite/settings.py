5002b95b965e65e245ab72d3a8e6c3a5f7fe8a8f
abhilash <abhilash29.1994@gmail.com>
2018-01-03 23:35:42 +0530

    Add existing file

--- blog_project/mysite/settings.py
+++ blog_project/mysite/settings.py
@@ -16,7 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR,'blog/templates/blog')
 
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
